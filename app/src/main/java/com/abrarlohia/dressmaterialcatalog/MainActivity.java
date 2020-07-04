f9ee05868632d2b11a76c0cdbf229c192e781add
yashraj-26 <yashujadeja26@gmail.com>
2020-07-04 12:58:42 +0530

    AppDrawer
    ContactUs-ready,(but onclicklister is left)
    About Us - Is not ready
    Share - is almost ready but not tested
    Rate - Right now i have put whatspp link for rating in playstore

--- app/src/main/java/com/abrarlohia/dressmaterialcatalog/MainActivity.java
+++ app/src/main/java/com/abrarlohia/dressmaterialcatalog/MainActivity.java
@@ -3,15 +3,21 @@ package com.abrarlohia.dressmaterialcatalog;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.Window;
 
 import com.abrarlohia.fragmets.AboutUsFragment;
 import com.abrarlohia.fragmets.ContactUsFragment;
@@ -45,6 +51,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
+
 //       if(savedInstanceState==null){
 //           getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
 //                   new SearchFragment()).commit();
@@ -56,7 +63,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.nav_home:
-                startActivity(new Intent(this,MainActivity.class));
+                startActivity(new Intent(this, MainActivity.class));
                 finish();
                 break;
             case R.id.nav_about_us:
@@ -68,16 +75,18 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                         new ContactUsFragment()).commit();
                 break;
             case R.id.nav_rate:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                        new RateFragment()).commit();
+//                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+//                        new RateFragment()).commit();
+                showRateDialog(this);
                 break;
             case R.id.nav_search:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                         new SearchFragment()).commit();
                 break;
             case R.id.nav_share:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                        new ShareFragment()).commit();
+//                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+//                        new ShareFragment()).commit();
+                ApplicationShareFragment();
                 break;
             case R.id.nav_videos:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
@@ -105,6 +114,56 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         return true;
     }
 
+    //TODO:Rate on playstore activity
+
+    public static void showRateDialog(final Context context) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context)
+                .setTitle("Rate application")
+                .setMessage("Would you like to rate the app at PlayStore")
+                .setPositiveButton("RATE", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (context != null) {
+                            String link = "https://play.google.com/store/apps/details?id=com.whatsapp";
+                            //String link = "market://details?id=";
+                            try {
+                                // play market available
+                                context.getPackageManager()
+                                        .getPackageInfo("com.android.vending", 0);
+                                // not available
+                            } catch (PackageManager.NameNotFoundException e) {
+                                e.printStackTrace();
+                                // should use browser
+                                link = "https://play.google.com/store/apps/details?id=";
+                            }
+                            // starts external action
+                            context.startActivity(new Intent(Intent.ACTION_VIEW,
+                                    Uri.parse(link + context.getPackageName())));
+                        }
+                    }
+                })
+                .setNegativeButton("CANCEL", null);
+        builder.show();
+    }
+
+
+    //TODO: Application share
+    private void ApplicationShareFragment() {
+        try {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Dress Catalog");
+            String shareMessage = "\nCheck this awesome application\n\n";
+            shareMessage = shareMessage + "https://play.google.com/store/apps/details?id=" + BuildConfig.APPLICATION_ID + "\n\n";
+            shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage);
+            startActivity(Intent.createChooser(shareIntent, "choose one"));
+
+        } catch (Exception e) {
+            //e.toString();
+        }
+    }
+
+    //TODO:Drawer
     @Override
     public void onBackPressed() {
         if (drawer.isDrawerOpen(GravityCompat.START)) {
